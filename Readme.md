ed76244f6a00a7c131edce0a9f3427946f09d020
Nils Ratnaweera <rata@zhaw.ch>
2022-06-08 15:48:59 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+
+
+Outline: 20 minuten
+
+Key Message: Nutzt RMarkdown / Bookdown für eure Projektberichte!
+
+Storyline: 
+- 3 min. Nachteile von Docx, pdf und sogar R Scrpts
+- 7 min. Grundlagen von Markdown, RMarkdown und Bookdown (Quarto?)
+- 5 min. Publizieren mit Github (Hosten auf zhaw.ch?) / Demo Wieselundco Bericht
+- 5 min. Kurzeinführung R-Packages
---
+++ report-to-png.sh
@@ -0,0 +1,9 @@
+% https://askubuntu.com/questions/50170/how-to-convert-pdf-to-image
+
+pdftoppm report.pdf report09 -singlefile -f 10 -png
+pdftoppm report.pdf report10 -singlefile -f 10 -png
+pdftoppm report.pdf report11 -singlefile -f 11 -png
+pdftoppm report.pdf report15 -singlefile -f 15 -png
+pdftoppm report.pdf report16 -singlefile -f 16 -png
+pdftoppm simple_regression.pdf simpleregression37 -singlefile -f 37 -png
+pdftoppm simple_regression.pdf simpleregression38 -singlefile -f 38 -png
